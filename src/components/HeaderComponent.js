b422f4b54fbe02aa8ad1cf98920f4055e7024866
Tarik Joullanar <aerro47@gmail.com>
2020-12-04 12:36:04 -0600

    Started conversion

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,67 @@
-import React from 'react';
-import { Button, Collapse, Jumbotron, Nav, NavbarBrand, Navbar, NavbarToggler, NavItem,
-Modal, ModalHeader, ModalBody } from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Collapse, Jumbotron, Nav, Navbar, NavbarToggler, NavItem,
+Modal, ModalHeader, ModalBody, NavLink } from 'reactstrap';
 
-function Header() {
-    return (
-        <React.Fragment>
-            <Jumbotron fluid>
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <h1>A Healthier Lifestyle</h1>
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            isNavOpen: false
+        };
+
+        this.toggleNav = this.toggleNav.bind(this);
+    };
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render () {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <div className="col-4 col-sm-3 col-md-2 align-self-center">
+                                    <img src="../images/Active Energy logo.png" className="img-fluid" alt="Logo for Active Energy" height="200" />
+                                </div>
+                                <h1>A Healthier Lifestyle</h1>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Jumbotron>
-        </React.Fragment>
-    );
+                </Jumbotron>
+
+                <Navbar expand="sm" sticky="top">
+                    <div className="container">
+                        <div className="col-md-10">
+                            <NavbarToggler onClick={this.toggleNav} />
+                            <Collapse isNavOpen={this.state.isNavOpen} navbar>
+                                <Nav navbar>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/home">Home</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/ourmenu">Our Menu</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/location">Location</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className="nav-link" to="/aboutus">About Us</NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </Collapse>
+                        </div>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
 }
+
+export default Header;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,16 +2,8 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        };
-    }
 
     render () {
-
 
         return(
             <React.Fragment>
